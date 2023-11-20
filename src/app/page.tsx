3fd6c5dc8695452f42a2f886042414c42a0178e7
Babatunde Alfred Olorundajo <tundealfred.office@gmail.com>
2023-11-20 23:28:15 +0000

    added style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-300 font-mono">
-      <h1 className="text-3xl font-bold underline">About me</h1>
+      <h1 className="text-4xl font-bold">About me</h1>
       <img src="https://avatars.githubusercontent.com/u/31713698?s=400&u=d9c4ab5f8ad668e418862533a4c8300fd3ad1b98&v=4" />
-      <p>
+      <h2 className="text-2xl text-bold">Babatunde Alfred O.</h2>
+      <h2 className="text-2xl underline p-10">Summary</h2>
+      <p className="text-justify">
         I am a reliable team player, I bring exceptional resourcefulness and a
         knack for creative problem-solving to the table. My expertise extends to
         effective communication with customers, fellow employees, and
